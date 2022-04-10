95ad1cd1e3bbcbc962c8281791006bbb1c7c5c7f
nikitariusov <nikitariusov@gmail.com>
2022-04-10 19:01:37 +0300

    init: create class Product

---
+++ main.py
@@ -0,0 +1,9 @@
+class Product:
+
+    def __init__(self, name, kty_link, competitors_links):
+        self.name = name
+        self.kty_link = kty_link
+        self.competitors_links = competitors_links
+        self.kty_price = None
+        self.competitors_price = None
+
