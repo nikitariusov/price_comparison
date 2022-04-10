3c602244178956ff50db878bd934f60c971b7870
nikitariusov <nikitariusov@gmail.com>
2022-04-10 23:54:20 +0300

    Откорректировал эксель файл, Вместо класса написал словарь, Создана функция чтения эксель файла

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+from read import read_file
+
 file = 'test/prods.xlsx'
 
-class Product:
+# class Product:
+#
+#     def __init__(self, name, kty_link, kty_price,
+#                  competitor_1, competitor__link_1, competitor_1_price,
+#                  competitor_2, competitor__link_2, competitor_2_price,
+#                  competitor_3, competitor__link_3, competitor_3_price):
+#         self.name = name
+#         self.kty_link = kty_link
+#         self.kty_price = kty_price
+#         self.competitor_1 = competitor_1
+#         self.competitor_link_1 = competitor__link_1
+#         self.competitor_1_price = competitor_1_price
+#         self.competitor_2 = competitor_2
+#         self.competitor_link_2 = competitor__link_2
+#         self.competitor_2_price = competitor_2_price
+#         self.competitor_3 = competitor_3
+#         self.competitor_link_3 = competitor__link_3
+#         self.competitor_3_price = competitor_3_price
 
-    def __init__(self, name, kty_link, competitor_1, competitor__link_1,
-                 competitor_2, competitor__link_2, competitor_3, competitor__link_3,):
-        self.name = name
-        self.kty_link = kty_link
-        self.competitor_1 = competitor_1
-        self.competitor_link_1 = competitor__link_1
-        self.competitor_2 = competitor_2
-        self.competitor_link_2 = competitor__link_2
-        self.competitor_3 = competitor_3
-        self.competitor_link_3 = competitor__link_3
-        self.kty_price = None
-        self.competitor_1_price = None
-        self.competitor_2_price = None
-        self.competitor_3_price = None
 
+if __name__ == '__main__':
+    print(read_file(file))
---
+++ read.py
@@ -0,0 +1,29 @@
+import openpyxl
+
+
+def read_file(file):
+    wb = openpyxl.load_workbook(file)
+    sheet = wb.active
+    rows = sheet.max_row
+    cols = sheet.max_column
+
+    name_cols = ['Название', 'Бренд', 'Ссылка КТУ', 'Цена КТУ',
+                 'Конкурент 1', 'Ссылка конкурента 1', 'Цена конкурента 1',
+                 'Конкурент 2', 'Ссылка конкурента 2', 'Цена конкурента 2',
+                 'Конкурент 3', 'Ссылка конкурента 3', 'Цена конкурента 3']
+
+    contents = []
+    for row in sheet.iter_rows(min_row=2, max_col=sheet.max_column, max_row=sheet.max_row):
+        items = {}
+        j = 0
+        for cell in row:
+            item = cell.value
+            items[name_cols[j]] = item
+            j += 1
+        contents.append(items)
+    return contents
+
+
+if __name__ == '__main__':
+    file = 'test/prods.xlsx'
+    print(read_file(file))
