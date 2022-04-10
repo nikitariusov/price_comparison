9d326756b72710d0e815efa2d236092fc7603f5c
nikitariusov <nikitariusov@gmail.com>
2022-04-10 19:46:48 +0300

    изменен класс Product - чтоб в таблице можно было в разных ячейках указать 3х конкурентов

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 class Product:
 
-    def __init__(self, name, kty_link, competitors_links):
+    def __init__(self, name, kty_link, competitor_1, competitor__link_1,
+                 competitor_2, competitor__link_2, competitor_3, competitor__link_3,):
         self.name = name
         self.kty_link = kty_link
-        self.competitors_links = competitors_links
+        self.competitor_1 = competitor_1
+        self.competitor_link_1 = competitor__link_1
+        self.competitor_2 = competitor_2
+        self.competitor_link_2 = competitor__link_2
+        self.competitor_3 = competitor_3
+        self.competitor_link_3 = competitor__link_3
         self.kty_price = None
-        self.competitors_price = None
+        self.competitor_1_price = None
+        self.competitor_2_price = None
+        self.competitor_3_price = None
 
