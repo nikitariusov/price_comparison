95bd2e2bf2bc86cd2e7e35d7915c656d1cb8dc82
nikitariusov <nikitariusov@gmail.com>
2022-04-11 21:11:38 +0300

    Добавлены retern функций. Добавлен парсер Гейзера и Ватерпомпа

--- pars.py
+++ pars.py
@@ -30,19 +30,32 @@ def parse(url):
 
     if html.status_code == 200:
         name = define_magazine(url)
-        if name == 'kty.com.ua':
-            kty_pars(html.text)
-        elif name == 'aquatools.com.ua':
-            aquatools_pars(html.text)
-        elif name == '3metra.com':
-            three_metra_pars(html.text)
-        elif name == 'zaslonka.com.ua':
-            zaslonka_pars(html.text)
-        elif name == 'palladium.ua':
-            palladim_pars(html.text)
-        elif name == 'in-ua.com':
-            install_ua(html.text)
 
+        if name == 'kty.com.ua':
+            price = kty_pars(html.text)
+        elif name == 'aquatools.com.ua':
+            price = aquatools_pars(html.text)
+        elif name == '3metra.com':
+            price = three_metra_pars(html.text)
+        elif name == 'zaslonka.com.ua':
+            price = zaslonka_pars(html.text)
+        elif name == 'palladium.ua':
+            price = palladim_pars(html.text)
+        elif name == 'in-ua.com':
+            price = install_ua(html.text)
+        elif name == 'geyser.com.ua':
+            price = geyser(html.text)
+        elif name == 'water-pomp.com.ua':
+            price = water_pomp(html.text)
+
+        else:
+            price = 'ERROR No Parser'
+        print(price)
+        return price
+    else:
+        price = 'ERROR no 200'
+        print(price)
+        return price
 
 
 def kty_pars(html_text):
@@ -53,8 +66,8 @@ def kty_pars(html_text):
         price = item.text
         price = also_number(price)
     else:
-        price = 'ERROR'
-    print(price)
+        price = 'PARS ERROR'
+    return price
 
 
 def aquatools_pars(html_text):
@@ -65,8 +78,8 @@ def aquatools_pars(html_text):
         price = item.text
         price = also_number(price)
     else:
-        price = 'ERROR'
-    print(price)
+        price = 'PARS ERROR'
+    return price
 
 
 def three_metra_pars(html_text):
@@ -77,8 +90,8 @@ def three_metra_pars(html_text):
         price = item.text
         price = also_number(price)
     else:
-        price = 'ERROR'
-    print(price)
+        price = 'PARS ERROR'
+    return price
 
 
 def zaslonka_pars(html_text):
@@ -89,8 +102,8 @@ def zaslonka_pars(html_text):
         price = item.text
         price = also_number(price)
     else:
-        price = 'ERROR'
-    print(price)
+        price = 'PARS ERROR'
+    return price
 
 
 def palladim_pars(html_text):
@@ -101,8 +114,8 @@ def palladim_pars(html_text):
         price = item.text
         price = also_number(price)
     else:
-        price = 'ERROR'
-    print(price)
+        price = 'PARS ERROR'
+    return price
 
 
 def install_ua(html_text):
@@ -113,41 +126,71 @@ def install_ua(html_text):
         price = item.text
         price = price[:price.find('.')].strip()
     else:
-        price = 'ERROR'
-    print(price)
+        price = 'PARS ERROR'
+    return price
+
+
+def geyser(html_text):
+    print('[TEST] Гейзер начало парсинга')
+    soup = BeautifulSoup(html_text, 'html.parser')
+    item = soup.find('div', class_='product-price__item')
+    if item:
+        price = item.text
+        price = price[:price.find('.')].strip()
+        price = also_number(price)
+    else:
+        price = 'PARS ERROR'
+    return price
+
+
+def water_pomp(html_text):
+    print('[TEST] Ватер начало парсинга')
+    soup = BeautifulSoup(html_text, 'html.parser')
+    item = soup.find('p', class_='b-product-cost__price')
+    if item:
+        price = item.text
+        price = price[:price.find('.')].strip()
+        price = also_number(price)
+    else:
+        price = 'PARS ERROR'
+    return price
 
 
 if __name__ == '__main__':
-    test_list = [{'Название': 'Насосная станция Optima TPS60 Mini', 'Бренд': 'Optima',
+    test_list = [{'Название': 'Optima TPS60 Mini', 'Бренд': 'Optima',
                   'Ссылка КТУ': 'https://kty.com.ua/ru/nasosnaya-stanciya-optima-tps60-mini-037-kvt.html',
                   'Цена КТУ': None, 'Конкурент 1': 'AquaTools',
                   'Ссылка конкурента 1': 'https://aquatools.com.ua/nasosnaya-stanciya-optima-tps-60-mini.html',
                   'Цена конкурента 1': None, 'Конкурент 2': '3 метра',
-                  'Ссылка конкурента 2': 'https://3metra.com/catalog/nasosnye-stantsii/nasosnaya-stantsiya-optima-ips-1-smart-14283/',
+                  'Ссылка конкурента 2': 'https://3metra.com/catalog/nasosnye-stantsii/nasosnaya-stantsiya-optima-tps60-mini-8400/',
                   'Цена конкурента 2': None, 'Конкурент 3': 'Заслонка',
                   'Ссылка конкурента 3': 'https://zaslonka.com.ua/nasosnaya-stanciya-optima-tps60-mini/',
-                  'Цена конкурента 3': None}, {'Название': 'Optima PC59', 'Бренд': 'Optima',
+                  'Цена конкурента 3': None, 'Конкурент 4': 'Гейзер',
+                  'Ссылка конкурента 4': 'https://geyser.com.ua/nasosnaya-stantsiya-optima-tps60-mini/',
+                  'Цена конкурента 4': None, 'Конкурент 5': 'Ватерпомп',
+                  'Ссылка конкурента 5': 'https://water-pomp.com.ua/p78671483-nasosnaya-stantsiya-optima.html',
+                  'Цена конкурента 5': None}, {'Название': 'Optima PC59', 'Бренд': 'Optima',
                                                'Ссылка КТУ': 'https://kty.com.ua/ru/zashhita-sukhogo-khoda-optima-pc59-c-reguliruemym-diapazonom-davleniya.html',
                                                'Цена КТУ': None, 'Конкурент 1': '3 метра',
                                                'Ссылка конкурента 1': 'https://3metra.com/catalog/avtomatika_dlya_nasosov/zashchita-sukhogo-khoda-optima-pc59-n-c-reguliruemym-diapazonom-davleniya/',
                                                'Цена конкурента 1': None, 'Конкурент 2': 'Заслонка',
                                                'Ссылка конкурента 2': 'https://zaslonka.com.ua/rele-davleniya-optima-pc59-n-c-reguliruemym-diapazonom-davleniya/',
                                                'Цена конкурента 2': None, 'Конкурент 3': None,
-                                               'Ссылка конкурента 3': None, 'Цена конкурента 3': None}, {'Название': 'EP-1', 'Бренд': 'Optima',
-                                               'Ссылка КТУ': None,
-                                               'Цена КТУ': None, 'Конкурент 1': '3 метра',
-                                               'Ссылка конкурента 1': 'https://palladium.ua/optima_ep-1.html',
-                                               'Цена конкурента 1': None, 'Конкурент 2': 'Заслонка',
-                                               'Ссылка конкурента 2': 'https://in-ua.com/products/gidroakkumulyator-vertikalnyj-reflex-refix-de-7302000-12l-de-sinij-10-bar-membrana-ne-smennaya',
-                                               'Цена конкурента 2': None, 'Конкурент 3': None,
-                                               'Ссылка конкурента 3': None, 'Цена конкурента 3': None}]
+                                               'Ссылка конкурента 3': None, 'Цена конкурента 3': None,
+                                               'Конкурент 4': None, 'Ссылка конкурента 4': None,
+                                               'Цена конкурента 4': None, 'Конкурент 5': None,
+                                               'Ссылка конкурента 5': None, 'Цена конкурента 5': None}]
 
     for i in test_list:
         if i['Ссылка КТУ']:
-            parse(i['Ссылка КТУ'])
+            price = parse(i['Ссылка КТУ'])
         if i['Ссылка конкурента 1']:
-            parse(i['Ссылка конкурента 1'])
+            price = parse(i['Ссылка конкурента 1'])
         if i['Ссылка конкурента 2']:
-            parse(i['Ссылка конкурента 2'])
+            price = parse(i['Ссылка конкурента 2'])
         if i['Ссылка конкурента 3']:
-            parse(i['Ссылка конкурента 3'])
+            price = parse(i['Ссылка конкурента 3'])
+        if i['Ссылка конкурента 4']:
+            price = parse(i['Ссылка конкурента 4'])
+        if i['Ссылка конкурента 5']:
+            price = parse(i['Ссылка конкурента 5'])
