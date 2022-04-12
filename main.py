e1605727cab5e1c7416c4976a1bf675a25701897
nikitariusov <nikitariusov@gmail.com>
2022-04-12 19:03:31 +0300

    Добавил счетчик товаров

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pars import *
 from save import save_file
 import easygui
 
-
 message = '''Программа для мониторинга цен на интернет ресурсах.
 
     !!! Любое копирование кода или распространение программы будет считаться нарушением авторских прав !!!
@@ -30,6 +29,7 @@ error_choise_file = '''     ERROR ошибка выбора файла
 
     Выход - Enter'''
 
+
 def start_pars(i, link_name_cell, price_name_cell):
     try:
         if i[link_name_cell]:
@@ -40,19 +40,22 @@ def start_pars(i, link_name_cell, price_name_cell):
 
 
 def cell_parsing(data):
+    j = 1
     for i in data:
+        print(f'Парсинг {j} из {products_count} товаров...')
         start_pars(i, 'Ссылка КТУ', 'Цена КТУ')
         start_pars(i, 'Ссылка конкурента 1', 'Цена конкурента 1')
         start_pars(i, 'Ссылка конкурента 2', 'Цена конкурента 2')
         start_pars(i, 'Ссылка конкурента 3', 'Цена конкурента 3')
         start_pars(i, 'Ссылка конкурента 4', 'Цена конкурента 4')
         start_pars(i, 'Ссылка конкурента 5', 'Цена конкурента 5')
+        j += 1
     return data
 
 
 if __name__ == '__main__':
     print(message)
-    x = str(input())
+    # x = str(input())
     try:
         file = easygui.fileopenbox('Выберите файл')
         data = read_file(file)
