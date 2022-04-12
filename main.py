9927e1b1fd37086ee7ba6413c2b2bd7384e0b160
nikitariusov <nikitariusov@gmail.com>
2022-04-12 15:16:45 +0300

    Пофиксил проблему начала парсинга по несуществующей ячейке - создал функцию что проверяет ячейку.

--- main.py
+++ main.py
@@ -23,31 +23,23 @@ import easygui
 #         self.competitor_link_3 = competitor__link_3
 #         self.competitor_3_price = competitor_3_price
 
+def start_pars(i, link_name_cell, price_name_cell):
+    try:
+        if i[link_name_cell]:
+            price = parse(i[link_name_cell])
+            i[price_name_cell] = price
+    except KeyError:
+        pass
+
+
 def cell_parsing(data):
     for i in data:
-        if i['Ссылка КТУ']:
-            price = parse(i['Ссылка КТУ'])
-            i['Цена КТУ'] = price
-
-        if i['Ссылка конкурента 1']:
-            price = parse(i['Ссылка конкурента 1'])
-            i['Цена конкурента 1'] = price
-
-        if i['Ссылка конкурента 2']:
-            price = parse(i['Ссылка конкурента 2'])
-            i['Цена конкурента 2'] = price
-
-        if i['Ссылка конкурента 3']:
-            price = parse(i['Ссылка конкурента 3'])
-            i['Цена конкурента 3'] = price
-
-        if i['Ссылка конкурента 4']:
-            price = parse(i['Ссылка конкурента 4'])
-            i['Цена конкурента 4'] = price
-
-        if i['Ссылка конкурента 5']:
-            price = parse(i['Ссылка конкурента 5'])
-            i['Цена конкурента 5'] = price
+        start_pars(i, 'Ссылка КТУ', 'Цена КТУ')
+        start_pars(i, 'Ссылка конкурента 1', 'Цена конкурента 1')
+        start_pars(i, 'Ссылка конкурента 2', 'Цена конкурента 2')
+        start_pars(i, 'Ссылка конкурента 3', 'Цена конкурента 3')
+        start_pars(i, 'Ссылка конкурента 4', 'Цена конкурента 4')
+        start_pars(i, 'Ссылка конкурента 5', 'Цена конкурента 5')
     return data
 
 
